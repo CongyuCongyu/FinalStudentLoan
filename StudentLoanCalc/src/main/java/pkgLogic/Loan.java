60bad952c2b1c1b7eb4ff74229405d38a7e1a764
congyulu <congyulu@congyus-mbp.fios-router.home>
2019-12-11 13:57:58 -0500

    first commit

---
+++ StudentLoanCalc/src/main/java/pkgLogic/Loan.java
@@ -0,0 +1,95 @@
+package pkgLogic;
+
+import java.time.LocalDate;
+
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
+public class Loan {
+	
+	private LocalDate startDate;
+	private double LoanAmount;
+	private double LoanBalanceEnd;
+	private double InterestRate;
+	private double AdditionalPayment;
+	private int LoanPaymentCnt;
+	private boolean bCompoundingOption;
+	
+	
+//Two constructors
+	public Loan(LocalDate startDate, double loanAmount, double interestRate, double additionalPayment,
+			int loanPaymentCnt) {
+		super();
+		this.startDate = startDate;
+		LoanAmount = loanAmount;
+		InterestRate = interestRate;
+		AdditionalPayment = additionalPayment;
+		LoanPaymentCnt = loanPaymentCnt;
+		
+		bCompoundingOption=false;
+		LoanBalanceEnd = 0;
+	}
+	
+	
+	public Loan(LocalDate startDate, double loanAmount, double loanBalanceEnd, double interestRate,
+			double additionalPayment, int loanPaymentCnt, boolean bCompoundingOption) {
+		super();
+		this.startDate = startDate;
+		LoanAmount = loanAmount;
+		LoanBalanceEnd = loanBalanceEnd;
+		InterestRate = interestRate;
+		AdditionalPayment = additionalPayment;
+		LoanPaymentCnt = loanPaymentCnt;
+		this.bCompoundingOption = bCompoundingOption;
+	}
+
+//getter for the instance variables
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+
+	public double getLoanAmount() {
+		return LoanAmount;
+	}
+
+
+	public double getLoanBalanceEnd() {
+		return LoanBalanceEnd;
+	}
+
+
+	public double getInterestRate() {
+		return InterestRate;
+	}
+
+
+	public double getAdditionalPayment() {
+		return AdditionalPayment;
+	}
+
+
+	public int getLoanPaymentCnt() {
+		return LoanPaymentCnt;
+	}
+
+
+	public boolean isbCompoundingOption() {
+		return bCompoundingOption;
+	}
+
+    public double GetPMT() {
+		
+    	return  Math.abs(FinanceLib.pmt(this.getInterestRate()/12, 
+    			this.getLoanPaymentCnt(), 
+    			this.getLoanAmount(), 
+    			this.getLoanBalanceEnd(), 
+    			this.bCompoundingOption));		
+
+    }
+	
+	
+	
+	
+	
+
+}
--- StudentLoanCalc/src/main/java/pkgLogic/Payment.java
+++ StudentLoanCalc/src/main/java/pkgLogic/Payment.java
@@ -1,24 +1,71 @@
 package pkgLogic;
 
+import java.time.LocalDate;
+
 public class Payment {
 	
 	//TODO: I've accounted for PaymentNbr, you need to add all the other fields for the class
 	private int PaymentNbr;
-
-	
-	//TODO: Fix the constructor, add the fields you've added.
-	public Payment(int paymentNbr) {
-		super();
-		PaymentNbr = paymentNbr;
+    private LocalDate DueDate;
+    private double Payment;
+    private double AdditionalPayment;
+    private double InterestPayment;
+    private double Principle;
+    private double EndingBanlance;
+ 
+//One constructor     
+	public Payment(double beginningBalance, 
+			int paymentNbr, 
+			LocalDate dueDate, 
+			Loan loan) 
+	{
+		
+		this.PaymentNbr = paymentNbr;
+		this.DueDate = dueDate;
+		this.Payment = loan.GetPMT();
+		this.AdditionalPayment = loan.getAdditionalPayment();
+		
+		InterestPayment=beginningBalance * (loan.getInterestRate()/12);
+		
+		Principle=loan.GetPMT()+loan.getAdditionalPayment()-InterestPayment;
+		
+		EndingBanlance= beginningBalance - Principle;
+		
+		
 	}
 
+	
+//getters for the instance variables		
 	public int getPaymentNbr() {
 		return PaymentNbr;
 	}
 
-	public void setPaymentNbr(int paymentNbr) {
-		PaymentNbr = paymentNbr;
+	public LocalDate getDueDate() {
+		return DueDate;
 	}
+
+	public double getPayment() {
+		return Payment;
+	}
+
+	public double getAdditionalPayment() {
+		return AdditionalPayment;
+	}
+
+	public double getInterestPayment() {
+		return InterestPayment;
+	}
+
+	public double getPrinciple() {
+		return Principle;
+	}
+
+	public double getEndingBanlance() {
+		return EndingBanlance;
+	}
+
+    
+    
 	
-	//TODO: Add getters and setters for new fields.	
+	
 }
